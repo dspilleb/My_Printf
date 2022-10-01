1a634520560daa7cee606525ad5618fc00dd7d73
Dan <spillebeendanilo@gmail.com>
2022-10-01 17:38:16 +0200

    update

--- ft_printf.h
+++ ft_printf.h
@@ -1,10 +1,12 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINT_H
 
+#include <stddef.h>
+
 int ft_printf(const char *s, ...);
 void ft_putchar_fd(char c, int fd);
 void ft_putnbr_fd(int n, int fd);
 void ft_putstr_fd(char *s, int fd);
 int ft_strlen(char *str);
-
+void *ft_memcpy(void *dest, const void *src, size_t n);
 #endif
